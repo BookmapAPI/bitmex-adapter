3320bd4899aee662dfd35ff3abd11a599bd178f2
Andrey Radchenko <andry.radchenko@gmail.com>
2019-10-17 19:21:36 +0300

    fix: remove unused imports

--- bitmex-adapter/src/com/bookmap/plugins/layer0/bitmex/Provider.java
+++ bitmex-adapter/src/com/bookmap/plugins/layer0/bitmex/Provider.java
@@ -2,7 +2,6 @@ package com.bookmap.plugins.layer0.bitmex;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -10,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.stream.Collectors;
 
 import com.bookmap.plugins.layer0.bitmex.adapter.BmConnector;
 import com.bookmap.plugins.layer0.bitmex.adapter.BmInstrument;
